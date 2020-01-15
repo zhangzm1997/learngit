3aaff1101d8f6b3bf0c415edef681f9f4e63c7dc
Zemin <zhangzm1997@126.com>
2020-01-15 09:56:21 +0800

    idea test

---
+++ test/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int [] [] a = new int [3][4];
+        System.out.println("hello World");
+        for(int i =0;i<10;i++){
+            new Rock();
+        }
+    }
+}
+class Rock{
+    Rock(){
+        System.out.println("Rock");
+    }
+}
